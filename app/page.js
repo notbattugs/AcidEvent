03b1ac64cf4fbe95e965943f58cf6cd0c01d5757
Battugs Bayanbileg <battugs@Battugss-MacBook-Air.local>
2024-03-19 23:11:33 +0800

    footer

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import Image from 'next/image';
 import Header from '@/components/Header';
 import AWorldInTransition from '@/components/AWorldInTransition';
+import Footer from '@/components/Footer';
 export default function Home() {
   return (
     <div>
       <Header />
       <AWorldInTransition />
+      <div className="h-60"></div>
+      <Footer />
     </div>
   );
 }
