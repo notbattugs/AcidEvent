7351711d8828c7ab1d6e7b0b48d607cc986fe04d
Battugs Bayanbileg <battugs@Battugss-MacBook-Air.local>
2024-03-19 21:41:23 +0800

    za blchochde

--- components/AWorldInTransition.js
+++ components/AWorldInTransition.js
@@ -6,7 +6,7 @@ export default function AWorldInTransition() {
       <div className="w-screen h-3/4 bg-[url('https://i.ibb.co/Twjnsxk/image.png')] bg-center bg-contain bg-no-repeat flex justify-center items-center">
         <img
           src="https://assets-global.website-files.com/65adbc76df6108aab934a79a/65b792d4547f14fbc8909a2f_image%20(5).webp"
-          className="w-96 h-96"
+          className="w-96 h-9"
         ></img>
       </div>
     </div>
