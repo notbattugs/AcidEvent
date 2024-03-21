d1f70efd8350733c4bff499166764937d4891cdd
Ebo <xdeboyt12@gmail.com>
2024-03-21 22:54:05 +0800

    ebo

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 import Image from 'next/image';
 import Header from '@/components/Header';
 import AWorldInTransition from '@/components/AWorldInTransition';
+import Overview from '@/components/Overview';
+import Organised from '@/components/Organised';
 export default function Home() {
   return (
     <div>
       <Header />
       <AWorldInTransition />
+      <Overview />
+      <Organised />
     </div>
   );
 }
